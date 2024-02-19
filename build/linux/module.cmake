1e7707bd3958af1da258d2759323b2b23ef074ff
gunnerDgd <ifstudiomail@naver.com>
2024-02-19 22:16:18 +0900

    [Progress] Build Automation - CMake based.

--- build/linux/module.cmake
+++ build/linux/module.cmake
@@ -1,16 +1,61 @@
-function   (add_linux_module_target par_make par_name par_dir par_target_dir par_src)
-    include       (${popcorn-dir}/build/linux/make.cmake)
-    add_linux_make(${par_make} ${par_name})
+function   (add_kernel_module par_name par_kernel)
+    file (REMOVE ${CMAKE_BINARY_DIR}/${par_name})
+    file (APPEND ${CMAKE_BINARY_DIR}/${par_name} "obj-m += ${par_name}.o\n\n")
+    add_custom_command (
+        OUTPUT            ${par_name}.ko
+        COMMAND           cp ${CMAKE_BINARY_DIR}/${par_name} ${CMAKE_SOURCE_DIR}/Kbuild
+        COMMAND           make -C /lib/modules/${par_kernel}/build M=${CMAKE_SOURCE_DIR} modules
+        COMMAND           rm ${CMAKE_SOURCE_DIR}/Kbuild
+        WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+        DEPENDS           ${CMAKE_BINARY_DIR}/${par_name}
+        VERBATIM
+    )
 
-    get_target_property   (popcorn-src       popcorn       SOURCES)
-    get_target_property   (popcorn-linux-src popcorn-linux SOURCES)
-    get_target_property   (popcorn-dev-src   popcorn-dev   SOURCES)
-    get_target_property   (popcorn-sync-src  popcorn-sync  SOURCES)
+    add_custom_target (${par_name}-build DEPENDS ${par_name}.ko)
+    add_custom_target (
+        ${par_name}-clean
+        COMMAND cp ${CMAKE_BINARY_DIR}/${par_name} ${CMAKE_SOURCE_DIR}/Kbuild
+        COMMAND make -C /lib/modules/${par_kernel}/build M=${CMAKE_SOURCE_DIR} clean
+        COMMAND rm ${CMAKE_SOURCE_DIR}/Kbuild
+    )
 
-    add_linux_make_target (${par_make} ${par_name} "${par_dir}/src"            ${popcorn-src})
-    add_linux_make_target (${par_make} ${par_name} "${par_dir}/src"            ${popcorn-linux-src})
-    add_linux_make_target (${par_make} ${par_name} "${par_dir}/linux/src/dev"  ${popcorn-dev-src})
-    add_linux_make_target (${par_make} ${par_name} "${par_dir}/linux/src/sync" ${popcorn-sync-src})
-    add_linux_make_target (${par_make} ${par_name} "${par_target_dir}"         ${par_src})
-    add_linux_make_command(${par_make} ${par_name})
+    add_custom_target (
+        ${par_name}-install
+        COMMAND cp ${CMAKE_BINARY_DIR}/${par_name} ${CMAKE_SOURCE_DIR}/Kbuild
+        COMMAND make -C /lib/modules/${par_kernel}/build M=${CMAKE_SOURCE_DIR} modules_install
+        COMMAND rm ${CMAKE_SOURCE_DIR}/Kbuild
+    )
+
+    if   (PRESET_ARCH_X86_64)
+        add_kernel_module_definition(${par_name} PRESET_LINUX)
+        add_kernel_module_definition(${par_name} PRESET_ARCH_X86_64)
+        add_kernel_module_definition(${par_name} PRESET_ARCH_BIT=64)
+    endif()
+endfunction()
+
+function   (add_kernel_module_include par_name par_path)
+    file(APPEND ${CMAKE_BINARY_DIR}/${par_name} "ccflags-y += -I${par_path}\n\n")
+endfunction()
+
+function   (add_kernel_module_definition par_name par_def)
+    file(APPEND ${CMAKE_BINARY_DIR}/${par_name} "ccflags-y += -D${par_def}\n\n")
+endfunction()
+
+function    (add_kernel_module_target par_name)
+    foreach (par_target IN LISTS ARGN)
+        get_target_property(par_path ${par_target} MODULE_PATH)
+        get_target_property(par_src  ${par_target} SOURCES)
+        file(APPEND ${CMAKE_BINARY_DIR}/${par_name} "${par_name}-objs += \\\n")
+        foreach    (src ${par_src})
+            string (FIND ${src} ".h" src_type REVERSE)
+            if   (NOT src_type EQUAL -1)
+                continue()
+            endif()
+            string(REPLACE ".c" ".o" src ${src})
+            file  (APPEND ${CMAKE_BINARY_DIR}/${par_name} "\t${par_path}/${src}\\")
+            file  (APPEND ${CMAKE_BINARY_DIR}/${par_name} "\n")
+        endforeach ()
+        file  (APPEND ${CMAKE_BINARY_DIR}/${par_name} "\n\n")
+    endforeach ()
+    file  (APPEND ${CMAKE_BINARY_DIR}/${par_name} "\n\n")
 endfunction()
--- build/module.cmake
+++ build/module.cmake
@@ -1,12 +1,6 @@
-function   (add_module_target par_make par_name par_dir par_target_dir par_src)
-    if   (UNIX AND NOT APPLE)
-        include                (${popcorn-dir}/build/linux/module.cmake)
-        add_linux_module_target(
-            ${par_make}
-            ${par_name}
-            ${par_dir}
-            ${par_target_dir}
-            ${par_src}
-        )
-    endif()
-endfunction()
+define_property(TARGET PROPERTY MODULE_KERNEL_VERSION BRIEF_DOCS "Target Kernel Version of the Module")
+define_property(TARGET PROPERTY MODULE_PATH           BRIEF_DOCS "Target Source's Relative Path")
+
+if    (UNIX AND NOT APPLE)
+    include(${popcorn-dir}/build/linux/module.cmake)
+endif ()
