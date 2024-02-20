f1b0ebf9bbe5f26142e460570d6b2bfdf0a87e3a
gunnerDgd <ifstudiomail@naver.com>
2024-02-20 15:45:21 +0900

    [Progress] Build Automation - CMake based.

--- build/module.cmake
+++ build/module.cmake
@@ -1,5 +1,6 @@
 define_property(TARGET PROPERTY MODULE_KERNEL_VERSION BRIEF_DOCS "Target Kernel Version of the Module")
 define_property(TARGET PROPERTY MODULE_PATH           BRIEF_DOCS "Target Source's Relative Path")
+define_property(TARGET PROPERTY MODULE_NAME           BRIEF_DOCS "Target Source's Relative Path")
 
 if    (UNIX AND NOT APPLE)
     include(${popcorn-dir}/build/linux/module.cmake)
