abce44f1c50e1b6c89ebb5ee93fb53f07fb43ee8
gunnerDgd <ifstudiomail@naver.com>
2024-02-29 20:16:40 +0900

    [Update] Rebuilt project from "klever"

--- build/module.cmake
+++
@@ -1,7 +0,0 @@
-define_property(TARGET PROPERTY MODULE_KERNEL_VERSION BRIEF_DOCS "Target Kernel Version of the Module" FULL_DOCS "Target Kernel Version of the Module")
-define_property(TARGET PROPERTY MODULE_PATH           BRIEF_DOCS "Target Source's Relative Path"       FULL_DOCS "Target Source's Relative Path")
-define_property(TARGET PROPERTY MODULE_NAME           BRIEF_DOCS "Target Source's Relative Path"       FULL_DOCS "Target Source's Relative Path")
-
-if    (UNIX AND NOT APPLE)
-    include(${popcorn-dir}/build/linux/module.cmake)
-endif ()
