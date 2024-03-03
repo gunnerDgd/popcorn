97d040494a56d1fc48949d89fd4de05c93368303
gunnerDgd <ifstudiomail@naver.com>
2024-03-04 03:27:41 +0900

    [Fix] Duplicated klever Clone

---
+++ linux/src/fs/.control.o.cmd
@@ -0,0 +1,66 @@
+savedcmd_klever/chr/../../../../../popcorn/linux/src/fs/control.o := gcc -Wp,-MMD,klever/chr/../../../../../popcorn/linux/src/fs/.control.o.d -nostdinc -I./arch/x86/include -I./arch/x86/include/generated  -I./include -I./arch/x86/include/uapi -I./arch/x86/include/generated/uapi -I./include/uapi -I./include/generated/uapi -include ./include/linux/compiler-version.h -include ./include/linux/kconfig.h -include ./include/linux/compiler_types.h -D__KERNEL__ -fmacro-prefix-map=./= -std=gnu11 -fshort-wchar -funsigned-char -fno-common -fno-PIE -fno-strict-aliasing -mno-sse -mno-mmx -mno-sse2 -mno-3dnow -mno-avx -fcf-protection=branch -fno-jump-tables -m64 -falign-jumps=1 -falign-loops=1 -mno-80387 -mno-fp-ret-in-387 -mpreferred-stack-boundary=3 -mskip-rax-setup -mtune=generic -mno-red-zone -mcmodel=kernel -Wno-sign-compare -fno-asynchronous-unwind-tables -mindirect-branch=thunk-extern -mindirect-branch-register -mindirect-branch-cs-prefix -mfunction-return=thunk-extern -fno-jump-tables -mharden-sls=all -fpatchable-function-entry=16,16 -fno-delete-null-pointer-checks -O2 -fno-allow-store-data-races -fstack-protector-strong -ftrivial-auto-var-init=zero -fno-stack-clash-protection -pg -mrecord-mcount -mfentry -DCC_USING_FENTRY -falign-functions=16 -fstrict-flex-arrays=3 -fno-strict-overflow -fno-stack-check -fconserve-stack -Wall -Wundef -Werror=implicit-function-declaration -Werror=implicit-int -Werror=return-type -Werror=strict-prototypes -Wno-format-security -Wno-trigraphs -Wno-frame-address -Wno-address-of-packed-member -Wframe-larger-than=2048 -Wno-main -Wno-unused-but-set-variable -Wno-unused-const-variable -Wno-dangling-pointer -Wvla -Wno-pointer-sign -Wcast-function-type -Wno-array-bounds -Wno-alloc-size-larger-than -Wimplicit-fallthrough=5 -Werror=date-time -Werror=incompatible-pointer-types -Werror=designated-init -Wenum-conversion -Wno-unused-but-set-variable -Wno-unused-const-variable -Wno-restrict -Wno-packed-not-aligned -Wno-format-overflow -Wno-format-truncation -Wno-stringop-overflow -Wno-stringop-truncation -Wno-missing-field-initializers -Wno-type-limits -Wno-shift-negative-value -Wno-maybe-uninitialized -Wno-sign-compare -g -g -DDEBUG -DPRESET_ARCH_X86_64 -DPRESET_ARCH_BIT=64 -DPRESET_LINUX -I/mnt/sda1/popcorn/include -I/mnt/sda1/popcorn/linux/include    -DKBUILD_MODFILE='"klever/chr/chr"' -DKBUILD_BASENAME='"control"' -DKBUILD_MODNAME='"chr"' -D__KBUILD_MODNAME=kmod_chr -c -o klever/chr/../../../../../popcorn/linux/src/fs/control.o klever/chr/../../../../../popcorn/linux/src/fs/control.c  
+
+source_klever/chr/../../../../../popcorn/linux/src/fs/control.o := klever/chr/../../../../../popcorn/linux/src/fs/control.c
+
+deps_klever/chr/../../../../../popcorn/linux/src/fs/control.o := \
+  include/linux/compiler-version.h \
+    $(wildcard include/config/CC_VERSION_TEXT) \
+  include/linux/kconfig.h \
+    $(wildcard include/config/CPU_BIG_ENDIAN) \
+    $(wildcard include/config/BOOGER) \
+    $(wildcard include/config/FOO) \
+  include/linux/compiler_types.h \
+    $(wildcard include/config/DEBUG_INFO_BTF) \
+    $(wildcard include/config/PAHOLE_HAS_BTF_TAG) \
+    $(wildcard include/config/FUNCTION_ALIGNMENT) \
+    $(wildcard include/config/CC_IS_GCC) \
+    $(wildcard include/config/X86_64) \
+    $(wildcard include/config/ARM64) \
+    $(wildcard include/config/HAVE_ARCH_COMPILER_H) \
+    $(wildcard include/config/CC_HAS_ASM_INLINE) \
+  include/linux/compiler_attributes.h \
+  include/linux/compiler-gcc.h \
+    $(wildcard include/config/RETPOLINE) \
+    $(wildcard include/config/ARCH_USE_BUILTIN_BSWAP) \
+    $(wildcard include/config/SHADOW_CALL_STACK) \
+    $(wildcard include/config/KCOV) \
+  klever/chr/../../../../../popcorn/linux/src/fs/control.h \
+  /mnt/sda1/popcorn/include/obj.h \
+  /mnt/sda1/popcorn/include/mem.h \
+  /mnt/sda1/popcorn/include/type.h \
+  include/linux/stdarg.h \
+  include/linux/stddef.h \
+  include/uapi/linux/stddef.h \
+  include/linux/types.h \
+    $(wildcard include/config/HAVE_UID16) \
+    $(wildcard include/config/UID16) \
+    $(wildcard include/config/ARCH_DMA_ADDR_T_64BIT) \
+    $(wildcard include/config/PHYS_ADDR_T_64BIT) \
+    $(wildcard include/config/64BIT) \
+    $(wildcard include/config/ARCH_32BIT_USTAT_F_TINODE) \
+  include/uapi/linux/types.h \
+  arch/x86/include/generated/uapi/asm/types.h \
+  include/uapi/asm-generic/types.h \
+  include/asm-generic/int-ll64.h \
+  include/uapi/asm-generic/int-ll64.h \
+  arch/x86/include/uapi/asm/bitsperlong.h \
+  include/asm-generic/bitsperlong.h \
+  include/uapi/asm-generic/bitsperlong.h \
+  include/uapi/linux/posix_types.h \
+  arch/x86/include/asm/posix_types.h \
+    $(wildcard include/config/X86_32) \
+  arch/x86/include/uapi/asm/posix_types_64.h \
+  include/uapi/asm-generic/posix_types.h \
+  /mnt/sda1/popcorn/include/ops.h \
+  /mnt/sda1/popcorn/include/ops/arith.h \
+  /mnt/sda1/popcorn/include/ops/../type.h \
+  /mnt/sda1/popcorn/include/ops/bit.h \
+  /mnt/sda1/popcorn/include/ops/cmp.h \
+  /mnt/sda1/popcorn/include/ops/logical.h \
+  /mnt/sda1/popcorn/include/ops/cast.h \
+  /mnt/sda1/popcorn/include/fut.h \
+  klever/chr/../../../../../popcorn/linux/src/fs/file.h \
+
+klever/chr/../../../../../popcorn/linux/src/fs/control.o: $(deps_klever/chr/../../../../../popcorn/linux/src/fs/control.o)
+
+$(deps_klever/chr/../../../../../popcorn/linux/src/fs/control.o):
