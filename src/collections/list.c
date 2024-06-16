9a4632b5e6be5752545702191f6b17b2846362e8
gunnerDgd <ifstudiomail@naver.com>
2024-06-17 05:36:34 +0900

    [Refactor] Parameter Naming Convention

--- src/collections/list.c
+++ src/collections/list.c
@@ -13,138 +13,106 @@ po_obj_trait* po_list_t = &po_list_trait;
 
 bool_t
     po_list_new
-        (po_list* par_list, u32_t par_count, va_list par)                    {
-            po_mem* mem = null_t; if (par_count > 0) mem = va_arg(par, void*);
+        (po_list* self, u32_t count, va_list arg)                        {
+            po_mem* mem = null_t; if (count > 0) mem = va_arg(arg, void*);
             if (po_trait_of(mem) != po_mem_t) mem = po_get_mem();
             if (po_trait_of(mem) != po_mem_t) return false_t    ;
 
-            if (!po_make_at(&par_list->begin, po_node) from(0)) goto new_err;
-            if (!po_make_at(&par_list->end  , po_node) from(0)) goto new_err;
-            if (!po_next_as(&par_list->begin, &par_list->end))  goto new_err;
-            par_list->mem = (po_mem*) po_ref(mem);
+            if (!po_make_at(&self->begin, po_node) from(0)) goto err;
+            if (!po_make_at(&self->end  , po_node) from(0)) goto err;
+            if (!po_next_as(&self->begin, &self->end))      goto err;
+            self->mem = (po_mem*) po_ref(mem);
             return true_t;
-    new_err:
-            po_del(&par_list->begin);
-            po_del(&par_list->end)  ;
+    err:    po_del(&self->begin);
+            po_del(&self->end)  ;
             return false_t;
 }
 
 bool_t
     po_list_clone
-        (po_list* par, po_list* par_clone)                                 {
-            if (!po_make_at(&par->begin, po_node) from(0))   return false_t;
-            if (!po_make_at(&par->end  , po_node) from(0))   return false_t;
+        (po_list* self, po_list* clone)                                     {
+            if (!po_make_at(&self->begin, po_node) from(0))   return false_t;
+            if (!po_make_at(&self->end  , po_node) from(0))   return false_t;
 
-            if (!po_next_as(&par->begin, &par_clone->end))   return false_t;
-            if (!po_prev_as(&par->end  , &par_clone->begin)) return false_t;
+            if (!po_next_as(&self->begin, &clone->end))   return false_t;
+            if (!po_prev_as(&self->end  , &clone->begin)) return false_t;
+            self->mem = clone->mem;
 
-            par->mem = par_clone->mem;
-            po_list_for(par_clone, push)                                 {
-                po_node* ret = po_make (po_node) from (1, po_value(push));
-                po_list_push_back(par, (po_obj*) ret);
-            }
-
+            po_list_for(clone, push) po_list_push_back(self, po_value(push));
             return true_t;
 }
 
 void
     po_list_del
-        (po_list* par)                               {
-            while(po_next(&par->begin) != &par->end) {
-                po_list_pop_back(par);
-            }
+        (po_list* self)                                           {
+            for ( ; !po_list_empty(self) ; po_list_pop_back(self));
 }
 
 po_node*
     po_list_push_back
-        (po_list* par, po_obj* par_push)                    {
-            if (!par_push)                     return null_t;
-            if (!par)                          return null_t;
-            if (po_trait_of(par) != po_list_t) return null_t;
-            po_node *ret = (po_node*) po_obj_new            (
-                par->mem,
+        (po_list* self, po_obj* push)                        {
+            if (po_trait_of(self) != po_list_t) return null_t;
+            if (po_trait_of(push) == null_t)    return null_t;
+            po_node *ret = (po_node*) po_obj_new             (
+                self->mem,
                 po_node_t,
                 1        ,
-                par_push
+                push
             );
 
             if (po_trait_of(ret) != po_node_t) return null_t;
-            po_prev_as(ret, po_prev(&par->end));
-            po_prev_as(&par->end, ret)         ;
+            po_prev_as(ret, po_prev(&self->end));
+            po_prev_as(&self->end, ret)         ;
             return  ret;
 }
 
 po_node*
     po_list_push_front
-        (po_list* par, po_obj* par_push)                    {
-            if (!par_push)                     return null_t;
-            if (!par)                          return null_t;
-            if (po_trait_of(par) != po_list_t) return null_t;
+        (po_list* self, po_obj* push)                        {
+            if (po_trait_of(self) != po_list_t) return null_t;
+            if (po_trait_of(push) == null_t)    return null_t;
             po_node *ret = (po_node*) po_obj_new            (
-                par->mem ,
+                self->mem,
                 po_node_t,
                 1        ,
-                par_push
+                push
             );
 
             if (po_trait_of(ret) != po_node_t) return null_t;
-            po_next_as(ret, po_next(&par->begin));
-            po_next_as(&par->begin, ret)         ;
+            po_next_as(ret, po_next(&self->begin));
+            po_next_as(&self->begin, ret)         ;
             return  ret;
 }
 
 po_node*
     po_list_move_back
-        (po_list* par, po_obj* par_push)                    {
-            if (!par_push)                     return null_t;
-            if (!par)                          return null_t;
-            if (po_trait_of(par) != po_list_t) return null_t;
-            po_node *ret = (po_node*) po_obj_new            (
-                par->mem,
-                po_node_t,
-                1        ,
-                par_push
-            );
+        (po_list* self, po_obj* push)                   {
+            po_node* ret = po_list_push_back(self, push);
 
             if (po_trait_of(ret) != po_node_t) return null_t;
-            po_del (par_push);
-
-            po_prev_as(ret, po_prev(&par->end));
-            po_prev_as(&par->end, ret)         ;
-            return  ret;
+            po_del(push);
+            return  ret ;
 }
 
 po_node*
     po_list_move_front
-        (po_list* par, po_obj* par_push)                    {
-            if (!par_push)                     return null_t;
-            if (!par)                          return null_t;
-            if (po_trait_of(par) != po_list_t) return null_t;
-            po_node *ret = (po_node*) po_obj_new            (
-                par->mem ,
-                po_node_t,
-                1        ,
-                par_push
-            );
+        (po_list* self, po_obj* push)                    {
+            po_node* ret = po_list_push_front(self, push);
 
             if (po_trait_of(ret) != po_node_t) return null_t;
-            po_del (par_push);
-
-            po_next_as(ret, po_next(&par->begin));
-            po_next_as(&par->begin, ret)         ;
-            return  ret;
+            po_del(push);
+            return  ret ;
 }
 
 po_obj*
     po_list_pop_front
-        (po_list* par)                                      {
-            if (!par)                          return null_t;
-            if (po_trait_of(par) != po_list_t) return null_t;
-            if (po_list_empty(par))            return null_t;
+        (po_list* self)                                        {
+            if (po_trait_of  (self) != po_list_t) return null_t;
+            if (po_list_empty(self))              return null_t;
 
-            po_node *ret_cur = po_next (&par->begin);
-            po_obj  *ret     = po_value(ret_cur)    ;
-            if (po_use_count(ret) == 1)             {
+            po_node *ret_cur = po_next (&self->begin);
+            po_obj  *ret     = po_value(ret_cur)     ;
+            if (po_use_count(ret) == 1)              {
                 po_del(ret_cur);
                 return 0;
             }
@@ -155,14 +123,13 @@ po_obj*
 
 po_obj*
     po_list_pop_back
-        (po_list* par)                                      {
-            if (!par)                          return null_t;
-            if (po_trait_of(par) != po_list_t) return null_t;
-            if (po_list_empty(par))            return null_t;
+        (po_list* self)                                        {
+            if (po_trait_of  (self) != po_list_t) return null_t;
+            if (po_list_empty(self))              return null_t;
 
-            po_node *ret_cur = po_prev (&par->end);
-            po_obj  *ret     = po_value(ret_cur)  ;
-            if (po_use_count(ret) == 1)           {
+            po_node *ret_cur = po_prev (&self->end);
+            po_obj  *ret     = po_value(ret_cur)   ;
+            if (po_use_count(ret) == 1)            {
                 po_del (ret_cur);
                 return null_t;
             }
@@ -173,26 +140,23 @@ po_obj*
 
 bool_t
     po_list_empty
-        (po_list* par)                                      {
-            if (!par)                          return true_t;
-            if (po_trait_of(par) != po_list_t) return true_t;
-            return po_next(&par->begin) == &par->end;
+        (po_list* self)                                      {
+            if (po_trait_of(self) != po_list_t) return true_t;
+            return po_next(&self->begin) == &self->end;
 }
 
 po_node*
     po_list_begin
-        (po_list* par)                                      {
-            if (!par)                          return null_t;
-            if (po_trait_of(par) != po_list_t) return null_t;
-            return par->begin.next;
+        (po_list* self)                                      {
+            if (po_trait_of(self) != po_list_t) return null_t;
+            return self->begin.next;
 }
 
 po_node*
     po_list_end
-        (po_list* par)                                      {
-            if (!par)                          return null_t;
-            if (po_trait_of(par) != po_list_t) return null_t;
-            return &par->end;
+        (po_list* self)                                      {
+            if (po_trait_of(self) != po_list_t) return null_t;
+            return &self->end;
 }
 
 #ifdef PRESET_LINUX
@@ -202,6 +166,8 @@ MODULE_LICENSE("GPL");
 EXPORT_SYMBOL(po_list_empty);
 EXPORT_SYMBOL(po_list_push_front);
 EXPORT_SYMBOL(po_list_push_back);
+EXPORT_SYMBOL(po_list_move_front);
+EXPORT_SYMBOL(po_list_move_back);
 EXPORT_SYMBOL(po_list_pop_front);
 EXPORT_SYMBOL(po_list_pop_back);
 EXPORT_SYMBOL(po_list_begin);
