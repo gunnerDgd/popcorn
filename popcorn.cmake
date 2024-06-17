350c70d21e13079cdd9aa218fdf778b36122e7b9
gunnerDgd <ifstudiomail@naver.com>
2024-06-17 15:53:40 +0900

    [Change] include_directories() added for automatic IDE Code Inspection

--- popcorn.cmake
+++ popcorn.cmake
@@ -4,3 +4,6 @@ set(POPCORN_ROOT ${CMAKE_CURRENT_LIST_DIR})
 include (${CMAKE_CURRENT_LIST_DIR}/script/feature.cmake)
 include (${CMAKE_CURRENT_LIST_DIR}/script/module.cmake)
 include (${CMAKE_CURRENT_LIST_DIR}/script/source.cmake)
+
+include_directories(/lib/modules/${CMAKE_HOST_SYSTEM_VERSION}/build/include)
+include_directories(${CMAKE_CURRENT_LIST_DIR}/include)
