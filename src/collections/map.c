f38ff6ff4dab3887f6803a5f3ed088305733ac5d
gunnerDgd <ifstudiomail@naver.com>
2024-07-23 22:03:39 +0900

    [Change] pp_map : Now require pp_cmp as first parameter for comparison

--- src/collections/map.c
+++ src/collections/map.c
@@ -2,13 +2,21 @@
 
 static bool_t
     do_new
-        (pp_map* self, u32_t count, va_list arg)                      {
-            pp_mem *mem = 0; if (count > 0) mem = va_arg(arg, pp_mem*);
+        (pp_map* self, u32_t count, va_list arg)                         {
+            pp_cmp *cmp = null_t; if (count > 0) cmp = va_arg(arg, any_t);
+            pp_mem *mem = null_t; if (count > 1) mem = va_arg(arg, any_t);
             if (pp_trait_of(mem) != pp_mem_t) mem = pp_get_mem();
             if (pp_trait_of(mem) != pp_mem_t) return false_t;
+            if (!cmp)                         return false_t;
 
-            if (!pp_make_at(&self->map, pp_list) from(1, mem)) return false_t;
+            if (!pp_make_at(&self->map, pp_list) from(1, mem)) goto err;
+            if (!cmp->ord)                                     goto err;
+            self->mem = (pp_obj*) pp_ref(mem);
+            self->cmp = cmp;
+
             return true_t;
+    err:    pp_del (&self->map);
+            return false_t;
 }
 
 static bool_t
@@ -38,13 +46,20 @@ pp_obj_trait* pp_map_t = &do_obj;
 
 pp_pos*
     pp_map_push
-        (pp_map* self, pp_obj* push)                        {
+        (pp_map* self, any_t push)                          {
             if (pp_trait_of(self) != pp_map_t) return null_t;
 
-            pp_list_for (&self->map, pos)                                   {
-                if (pp_eq_arg(push, pp_as_any((pp_obj*) pos))) return null_t;
-                if (pp_gt_arg(push, pp_as_any((pp_obj*) pos))) continue;
-                pp_pos *cur = pp_make (pp_pos) from (1, push);
+            pp_list_for (&self->map, pos)                                    {
+                pp_ord_t ord = self->cmp->ord(push, pp_as_any((pp_obj*) pos));
+                if (ord == pp_ord_err) return null_t;
+                if (ord == pp_ord_eq)  return null_t;
+                if (ord == pp_ord_gt)  continue;
+                pp_pos *cur = pp_obj_new      (
+                    self->mem,
+                    pp_pos_t ,
+                    1        ,
+                    push
+                );
 
                 if (pp_trait_of(cur) != pp_pos_t) return null_t;
                 return pp_next_as(cur, pos);
@@ -55,13 +70,13 @@ pp_pos*
 
 pp_pos*
     pp_map_move
-        (pp_map *self, pp_obj* push)                        {
+        (pp_map *self, any_t* push)                         {
             if (pp_trait_of(self) != pp_map_t) return null_t;
-            pp_pos *ret = pp_map_push(self, push);
+            pp_pos *ret = pp_map_push(self, *push);
 
             if (pp_trait_of(ret) != pp_pos_t) return null_t;
-            pp_del (push);
-            return  ret  ;
+            *push = null_t;
+            return ret;
 }
 
 void
@@ -79,8 +94,9 @@ pp_pos*
         (pp_map* self, any_t key)                           {
             if (pp_trait_of(self) != pp_map_t) return null_t;
 
-            pp_list_for (&self->map, pos)                             {
-                if (pp_ne_arg(pp_as_any((pp_obj*) pos), key)) continue;
+            pp_list_for (&self->map, pos)                                   {
+                pp_ord_t ord = self->cmp->ord(pp_as_any((pp_obj*) pos), key);
+                if (ord != pp_ord_eq) continue;
                 return pos;
             }
 
