d2d6a1d9e892ce58314fde0ddff4fa0c0c474262
gunnerDgd <ifstudiomail@naver.com>
2024-07-26 01:12:08 +0900

    [Fix] null_t missing

--- src/collections/pos.h
+++ src/collections/pos.h
@@ -29,9 +29,9 @@ pp_pos* pp_pos_next (pp_pos*, pp_pos*);
 pp_pos* pp_pos_prev (pp_pos*, pp_pos*);
 
 #ifndef __cplusplus
-#define pp_next(par)               pp_pos_next (par, 0)
+#define pp_next(par)               pp_pos_next (par, null_t)
 #define pp_next_as(par, par_next)  pp_pos_next (par, par_next)
-#define pp_prev(par)               pp_pos_prev (par, 0)
+#define pp_prev(par)               pp_pos_prev (par, null_t)
 #define pp_prev_as(par, par_prev)  pp_pos_prev (par, par_prev)
 #endif
 
